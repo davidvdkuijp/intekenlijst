efb5ac5e0d49039a613afa98bce9309d03efe7d2
Andrew <a.vigelandzoon@unasat.sr>
2018-06-17 22:40:14 -0700

    Andrew

--- src/main/java/sr/unasat/intekenlijst/app/Application.java
+++ src/main/java/sr/unasat/intekenlijst/app/Application.java
@@ -1,4 +1,13 @@
 package sr.unasat.intekenlijst.app;
 
+import sr.unasat.intekenlijst.entities.Student;
+import sr.unasat.intekenlijst.repositories.StudentRepository;
+
 public class Application {
+
+    public static void main(String[] args) {
+        StudentRepository studenRep = new StudentRepository();
+        List<Student> studentLis
+    }
 }
+
--- src/main/java/sr/unasat/intekenlijst/repositories/StudentRepository.java
+++ src/main/java/sr/unasat/intekenlijst/repositories/StudentRepository.java
@@ -1,4 +1,29 @@
 package sr.unasat.intekenlijst.repositories;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class StudentRepository {
+    private Connection connection;
+
+    public StudentRepository() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Het systeem werkt");
+
+            String URL = "jdbc:mysql://localhost/beroepsproduct";
+            String USER = "root";
+            String PASS = "admin"
+            connection = DriverManager.getConnection(URL,USER,PASS);
+            System.out.println(connection);
+        }    catch (ClassNotFoundException ex) {
+            System.out.println("Class can not load");
+            System.exit(1);
+        }   catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
+
