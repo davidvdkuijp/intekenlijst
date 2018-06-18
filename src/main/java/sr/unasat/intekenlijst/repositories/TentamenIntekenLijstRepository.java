85637713eb35c6249f43e5054cd3de595b4e805b
davidvdkuijp <39437907+davidvdkuijp@users.noreply.github.com>
2018-06-18 20:46:18 -0300

    Update TentamenIntekenLijstRepository.java

--- src/main/java/sr/unasat/intekenlijst/repositories/TentamenIntekenLijstRepository.java
+++ src/main/java/sr/unasat/intekenlijst/repositories/TentamenIntekenLijstRepository.java
@@ -1,4 +1,110 @@
 package sr.unasat.intekenlijst.repositories;
 
+
+import sr.unasat.intekenlijst.entities.Tentamenintekenlijst;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
 public class TentamenIntekenLijstRepository {
+
+    private Connection connection;
+
+    public TentamenIntekenLijstRepository() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Het systeem werkt");
+
+            String URL = "jdbc:mysql://localhost/beroepsproduct";
+            String USER = "root";
+            String PASS = "admin";
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println(connection);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Class can not load");
+            System.exit(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public List<Tentamenintekenlijst> findAllRecords() {
+        List<Tentamenintekenlijst> tentamenintekenlijstList = new ArrayList<Tentamenintekenlijst>();
+        Statement stmt = null;
+        try {
+            stmt = connection.createStatement();
+            String sql = "select * from Tentamenintekenlijst";
+            ResultSet rs = stmt.executeQuery(sql);
+            System.out.println("resultset: " + rs);
+            //STEP 5: Extract data from result set
+            while ( rs.next() ) {
+                //Retrieve by column name
+                int INTEKEN_ID = rs.getInt("INTEKEN_ID");
+                int STUDENTID = rs.getInt("STUDENTID");
+                String STUDENTNAAM = rs.getString("STUDENTNAAM");
+                String STUDENTVOORNAAM = rs.getString("STUDENTVOORNAAM");
+                int TENTAMENID = rs.getInt("TENTAMENID");
+                String TENTAMENNAAM = rs.getString("TENTAMENNAAM");
+                Date INTEKEN_DATUM = rs.getDate("INTEKEN_DATUM");
+                Date UITEKEN_DATUM = rs.getDate("UITEKEN_DATUM");
+                String INGETEKEND = rs.getString("INGETEKEND");
+           
+                
+                 /*    //Display values
+               System.out.print("ID: " + id);
+               System.out.print(", Age: " + naam);*/
+                tentamenintekenlijstList.add(new Tentamenintekenlijst(INTEKEN_ID, STUDENTID,STUDENTNAAM,STUDENTVOORNAAM,TENTAMENID,TENTAMENNAAM,INTEKEN_DATUM,UITEKEN_DATUM,INGETEKEND));
+                //  persoonList.add(new Persoon(rs.getInt("id"), rs.getString("naam")));
+            }
+            rs.close();
+
+
+        } catch (SQLException e) {
+
+        } finally {
+
+        }
+        return tentamenintekenlijstList;
+    }
+
+    public int insertOneRecord(Tentamenintekenlijst tentain) {
+        PreparedStatement stmt = null;
+        int result = 0;
+        try {
+            String sql = "insert into tentain (NAAM) values(?)";
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, tentain.getNAAM());
+            result = stmt.executeUpdate();
+            System.out.println("resultset: " + result);
+
+        } catch (SQLException e) {
+
+        } finally {
+
+        }
+        return result;
+    }
+
+    public int deleteOneRecord(Student student){
+        PreparedStatement stmt = null;
+        int result = 0;
+        try {
+            String sql = "DELETE FROM student WHERE student.STUDENTEN_ID = ?";
+            stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, student.getSTUDENT_ID());
+            result = stmt.executeUpdate();
+            System.out.println("deleted: " + student.getSTUDENT_ID());
+
+        } catch (SQLException e) {
+
+        } finally {
+
+        }
+        return result;
+    }
+
 }
