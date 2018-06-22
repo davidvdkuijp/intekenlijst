b2070e9fbb9a1fc28c843f0a4e9123c177f28fc2
davidvdkuijp <d.vanderkuijp@unasat.sr>
2018-06-21 21:52:08 -0300

    init

---
+++ src/main/java/sr/unasat/intekenlijst/app/InsertTentamen.java
@@ -0,0 +1,32 @@
+/*
+package sr.unasat.intekenlijst.app;
+
+import sr.unasat.intekenlijst.entities.Tentamen;
+import sr.unasat.intekenlijst.repositories.TentamenRepository;
+
+import java.util.List;
+
+public class InsertTentamen {
+
+    public static void main(String[] args) {
+
+        TentamenRepository tentamenRepo = new TentamenRepository();
+        List<Tentamen> tentamenList = tentamenRepo.insertOneRecord();
+        for (Tentamen tentamen : tentamenList) {
+
+            int pk = tentamenRepo.insertOneRecord(new Tentamen(48, "NW", "Netwerken", "2019-01-23", "08:00", "2 uren", "Regulier tentamen"));
+
+            System.out.println(tentamen);
+        }
+
+        */
+/*PersoonRepository persoonRepo = new PersoonRepository();
+        List<Persoon> persoonList = persoonRepo.findAllRecords();
+        for (Persoon persoon : persoonList) {
+            System.out.println(persoon);*//*
+
+
+
+    }
+}
+*/
---
+++ src/main/java/sr/unasat/intekenlijst/app/UpdateTentamenIntekenLijst.java
@@ -0,0 +1,31 @@
+/*
+package sr.unasat.intekenlijst.app;
+
+import sr.unasat.intekenlijst.entities.Tentamenintekenlijst;
+import sr.unasat.intekenlijst.repositories.TentamenIntekenLijstRepository;
+
+public class UpdateTentamenIntekenLijst {
+
+    public static void main(String[] args) {
+
+        TentamenIntekenLijstRepository tilRep = new TentamenIntekenLijstRepository();
+        Tentamenintekenlijst recordFound = tilRep.findAllRecords();
+        System.out.println("Record: " + tilRep);
+
+        recordFound.setSTUDENTNAAM("Josh");
+        tilRep.updateOneRecord(recordFound);
+
+
+
+        */
+/*ContactInformatieRepository ci = new ContactInformatieRepository();
+        ContactInformatie recordFound = ci.findOneRecord(1234, "Manjastraat 10");
+        System.out.println("single record: " + ci);
+
+       recordFound.setTelefoonNummer(8888);
+        recordFound.getPersoon().setId(4);
+       ci.updateOneRecord(recordFound);*//*
+
+    }
+}
+*/
